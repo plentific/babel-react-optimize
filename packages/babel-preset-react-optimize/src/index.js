7d430c9b92def4a67ad09971c1bffecdef07756c
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2019-11-28 18:34:36 +0100

    Upgrade to babel 7.

--- packages/babel-preset-react-optimize/src/index.js
+++ packages/babel-preset-react-optimize/src/index.js
@@ -1,9 +1,11 @@
-module.exports = {
-  plugins: [
-    require('babel-plugin-transform-react-constant-elements'),
-    require('babel-plugin-transform-react-inline-elements'),
-    require('babel-plugin-transform-react-remove-prop-types')['default'],
-    require('babel-plugin-transform-react-pure-class-to-function-fork'),
-    require('babel-plugin-transform-react-router-optimize')['default']
-  ]
+module.exports = function () {
+  return {
+    plugins: [
+      require('@babel/plugin-transform-react-constant-elements'),
+      require('@babel/plugin-transform-react-inline-elements'),
+      require('babel-plugin-transform-react-remove-prop-types')['default'],
+      require('babel-plugin-transform-react-pure-class-to-function-fork'),
+      require('babel-plugin-transform-react-router-optimize')['default']
+    ]
+  }
 };
